5a6692d13028267f8a5274b310bea505da2e8b5a
pedroribeirodev <pedro.bonfim.13@gmail.com>
2020-02-10 14:54:11 -0300

    Second commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Notifications from '../Notifications';
+
+import logo from '../../assets/logo_interna.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Pedro Ribeiro</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Pedro Ribeiro"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+      </Form>
+    </Container>
+  );
 }
